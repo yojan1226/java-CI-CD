7399d8c7a01a962909920059401c5ea41351becd
yojan1226 <yojansawdekar127@gmail.com>
2025-11-12 16:30:31 +0530

    rename file name

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java CI/CD demo!");
+        System.out.println("Hello from Java CI/CD demo-pipeline!");
     }
 
     public static String greet(String name) {
